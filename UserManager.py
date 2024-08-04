9267eb6b5b2504160d4e2bc91865d37008f8dd92
AltynbekMaksat <157010910+AltynbekMaksat@users.noreply.github.com>
2024-08-05 00:26:35 +0500

    2

--- UserManager.py
+++ UserManager.py
@@ -22,11 +22,23 @@ class UserManager:
             print("No user found with id ", {id})
             return None
         
-    
+    def deleteUser(self, id):
+        if id in UserManager.database.keys():
+            print(f"Deleting user with id : {id}")
+            del UserManager.database[id]
+            return True
+        else:
+            print("No user found with id ", {id}, " to delete ")
+            return False
         
-    
-        
-        
+    def findUserByName(self, name):
+        ans = []
+        for key1, name1 in UserManager.database.items():
+            if name1 == name:
+                ans.append(key1)
+                
+        return ans
+                
         
         
 
@@ -37,6 +49,11 @@ obj = UserManager()
 obj.addUser("Jaras")
 obj.addUser("Miras")
 obj.addUser("Erasyl")
+obj.addUser("Miras")
 
 obj.getUser(2)
 obj.getUser(5)
+
+print(obj.deleteUser(3))
+
+print(obj.findUserByName("Erasyl"))
